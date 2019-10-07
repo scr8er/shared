3f9ca6d43e4927ac735a2ce00e3b6956e74d2bb0
scr8er <51819865+scr8er@users.noreply.github.com>
2019-10-07 12:05:58 +0200

    Update requisitos AzureADConnect.ps1

--- Azure/requisitos AzureADConnect.ps1
+++ Azure/requisitos AzureADConnect.ps1
@@ -1,6 +1,4 @@
 ﻿# REQUISITOS INSTALACIÓN AZURE AD CONNECT
 
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord 
-
-ConvertTo-ExcelXlsx -
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
